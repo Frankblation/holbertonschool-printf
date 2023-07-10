70b99f3cca08d0060a24687f63c58a79ae98c199
mokasp <125315163+mokasp@users.noreply.github.com>
2023-07-09 21:02:24 -0500

    betty formatting

--- printf.c
+++ printf.c
@@ -12,48 +12,48 @@ int _printf(const char *format, ...)
     va_start(args, format);
     char_count = 0;
     if (format != NULL)
+    {
+        while (*format)
         {
-            while (*format)
+            if (*format == '%')
             {
-                if (*format == '%')
+                format++;
+                if (*format == 'd' || *format == 'i')
                 {
-                    format++;
-                    if (*format == 'd' || *format == 'i')
-                    {
-                        char_count += integer(args);
-                    }
-                    else if (*format == 'c')
-                    {
-                        char_count += character(args);
-                    }
-                    else if (*format == 's')
-                    {
-                        char_count += string(args);
-                    }
-                    else if (*format == '%')
-                    {
-                        _putchar(*format);
-                        char_count += 2;;
-                    }
-                    else
-                    {
-                        _putchar('%');
-                        _putchar(*format);
-                        char_count += 2;
-                    }
+                    char_count += integer(args);
+                }
+                else if (*format == 'c')
+                {
+                    char_count += character(args);
+                }
+                else if (*format == 's')
+                {
+                    char_count += string(args);
+                }
+                else if (*format == '%')
+                {
+                    _putchar(*format);
+                    char_count += 2;;
                 }
                 else
                 {
+                    _putchar('%');
                     _putchar(*format);
-                    char_count++;
+                    char_count += 2;
                 }
-                format++;
             }
+            else
+            {
+                _putchar(*format);
+                char_count++;
+            }
+            format++;
         }
-        else
-        {
-            return (0);
-        }
-        va_end(args);
-        return (char_count);
+    }
+    else
+    {
+        return (0);
+    }
+    va_end(args);
+    return (char_count);
 }
