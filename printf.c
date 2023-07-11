17d0c338faab0fa058f97a7fce77215bf926261f
Frankblation <frankblation@gmail.com>
2023-07-10 21:32:08 -0700

    delete my file

--- printf.c
+++ printf.c
@@ -1,4 +1,4 @@
-#include "main.h"
+include "main.h"
 /**
  * _printf - print multiple types of data
  * @format: list of argument
@@ -10,48 +10,6 @@ int _printf(const char *format, ...)
 int char_count;
 va_list args;
 
-<<<<<<< HEAD
-    va_start(args, format);
-   
-    char_count = 0;
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            if (*format == 'd' || *format == 'i')
-            {
-                char_count += integer(args);
-            }
-            else if (*format == 'c')
-            {
-                char_count += character(args);
-            }
-            else if (*format == 's')
-            {
-               char_count += string(args);
-            }
-            else if (*format == '%')
-            {
-                char_count += percent(args);
-            }
-	    else
-	    {
-		_putchar('%');
-		_putchar(*format);
-	    }
-	}
-	else
-	{
-		_putchar(*format);
-		char_count++;
-	}
-    format++;
-    }
-    va_end(args);
-
-    return (char_count);
-=======
 va_start(args, format);
 char_count = 0;
 if (format != NULL && *format != '\0')
@@ -89,14 +47,9 @@ else
 {
 _putchar(*format);
 char_count++;
-}
-format++;
-}
-}
-else
-{
+
+
 return (0);
->>>>>>> 515fef3cc7181b259904b430fb221e7dec5b982e
 }
 va_end(args);
 return (char_count);
