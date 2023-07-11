8832ceba1f6671489f6aa165dd8233881924b159
Frankblation <124920027+Frankblation@users.noreply.github.com>
2023-07-10 23:43:14 -0500

    printf.c

---
+++ printf.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _printf - print multiple types of data
+ * @format: list of argument
+ *
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+
+int char_count;
+va_list args;
+char_count = 0;
+va_start(args, format);
+if (format == NULL)
+{
+return (-1);
+}
+else
+{
+char_count += helper(format, args);
+}
+va_end(args);
+return (char_count);
+}
